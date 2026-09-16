df8f7b94afdf77918d7e3959bfa33a9fe7c75fdf
Aygul-Ismayilova <Aygul.Ismayilova@edu.rtu.lv>
2026-09-16 10:16:41 +0000

    updated hello message added my name

--- src/hello.c
+++ src/hello.c
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[]) {
   // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  
   printf("You passed %d argument(s).\n", argc - 1);
+  printf("Hello from <Aygul Ismayilova>! \n");
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
   }
